dbb9afbb2818566e63465fbee870b2f50f9ce522
jt <balisongian@gmail.com>
2024-06-08 19:59:39 +0700

    asd

--- components/MiniGameSection/index.tsx
+++ components/MiniGameSection/index.tsx
@@ -5,7 +5,7 @@ const MiniGameSection = () => {
         <section id="miniGameSection" className="overflow-hidden pb-20 pt-15 lg:pb-25 xl:pb-30">
             <iframe
                 title='mini-game'
-                src="/surf-game/game-index.html"
+                src="/surf-game/game.htm"
                 className='w-full h-screen border-none'
                 allowFullScreen
                 loading='lazy'
