41f9cdb6997ede8321f7a506335b2191c6210712
jt <balisongian@gmail.com>
2024-06-29 20:13:33 +0700

    asd

--- components/Notion-CMS/TopScrollButton.tsx
+++ components/Notion-CMS/TopScrollButton.tsx
@@ -1,20 +1,22 @@
-// "use client";
+"use client";
 
-// import { MoveUp } from "lucide-react";
+import { MoveUp } from "lucide-react";
 
-// const ScrollToTopButton = () => {
-//   const { showTopButton, scrollToTop } = useScrollToTop();
+const ScrollToTopButton = () => {
+    //   const { showTopButton, scrollToTop } = useScrollToTop();
 
-//   return (
-//     <Button
-//       className={`fixed bottom-8 right-4 transition-opacity duration-300 rounded-full px-2 py-3 ${
-//         showTopButton ? "opacity-100" : "opacity-0"
-//       }`}
-//       onClick={scrollToTop}
-//     >
-//       <MoveUp />
-//     </Button>
-//   );
-// };
+    return (
+        // <Button
+        //   className={`fixed bottom-8 right-4 transition-opacity duration-300 rounded-full px-2 py-3 ${
+        //     showTopButton ? "opacity-100" : "opacity-0"
+        //   }`}
+        //   onClick={scrollToTop}
+        // >
+        //   <MoveUp />
+        // </Button>
+        <>
+        </>
+    );
+};
 
-// export default ScrollToTopButton;
+export default ScrollToTopButton;
