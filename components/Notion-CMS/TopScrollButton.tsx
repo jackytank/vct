05f94334f2943c7783d4d0e94567523875797eee
jt <balisongian@gmail.com>
2024-06-29 20:10:16 +0700

    [modify] add react-share

--- components/Notion-CMS/TopScrollButton.tsx
+++ components/Notion-CMS/TopScrollButton.tsx
@@ -1,22 +1,20 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/button";
-import useScrollToTop from "../app/hook/useScrollToTop";
-import { MoveUp } from "lucide-react";
+// import { MoveUp } from "lucide-react";
 
-const ScrollToTopButton = () => {
-  const { showTopButton, scrollToTop } = useScrollToTop();
+// const ScrollToTopButton = () => {
+//   const { showTopButton, scrollToTop } = useScrollToTop();
 
-  return (
-    <Button
-      className={`fixed bottom-8 right-4 transition-opacity duration-300 rounded-full px-2 py-3 ${
-        showTopButton ? "opacity-100" : "opacity-0"
-      }`}
-      onClick={scrollToTop}
-    >
-      <MoveUp />
-    </Button>
-  );
-};
+//   return (
+//     <Button
+//       className={`fixed bottom-8 right-4 transition-opacity duration-300 rounded-full px-2 py-3 ${
+//         showTopButton ? "opacity-100" : "opacity-0"
+//       }`}
+//       onClick={scrollToTop}
+//     >
+//       <MoveUp />
+//     </Button>
+//   );
+// };
 
-export default ScrollToTopButton;
+// export default ScrollToTopButton;
